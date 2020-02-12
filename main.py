64d9c22b1afc1024dd84525c5c69d8f35e0d1c2e
xutianshu <1577840123@qq.com>
2020-02-12 20:53:14 +0800

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+import os
+
+HEIGHT = 720
+WIDTH = 1280
+
+# Global constants here
+BLACK = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREY = (50, 50, 50)
+RED = (207, 0, 0)
+
+
+if __name__ == "__main__":
+    pygame.init()
