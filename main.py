9e2220ee3e43d1b09d92ca73becd7d8f1f071d7a
xutianshu <1577840123@qq.com>
2020-02-12 21:29:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import sys
 
 HEIGHT = 720
 WIDTH = 1280
@@ -10,6 +11,24 @@ BLACK = (0, 0, 0)
 GREY = (50, 50, 50)
 RED = (207, 0, 0)
 
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+pygame.init()
+pygame.display.set_caption("Poker")
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 
-if __name__ == "__main__":
-    pygame.init()
+a = 0
+
+font = pygame.font.Font('font/CoffeeTin.ttf', 50)
+
+while True:
+    vis = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit();
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            a = a + 1
+            vis = True
+    if vis == False:
+        continue
+    print(a)
